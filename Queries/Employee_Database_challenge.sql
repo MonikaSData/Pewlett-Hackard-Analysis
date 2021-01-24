23098f2894611e51458dfde0236d8fdbaaeccbcd
MonikaSData <mspiritova@gwmail.gwu.edu>
2021-01-24 16:28:17 -0500

    Adding new csv files (SQL Query)

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -23,3 +23,33 @@ INNER JOIN titles AS t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no, t.to_date DESC;
+
+
+-- The number of unique titles for employees by their most recent job title who are about to retire
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+
+-- List of employees titles that are retiring
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_employee AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+    AND (de.to_date = '9999-01-01')
+	AND (t.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+
